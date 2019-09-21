e49dc33e9f538e8418d870c142fa2ab7a40fff9f
darren lefcoe <dlefcoe@gmail.com>
2019-09-21 19:53:40 +0100

    Update read7readN.py

--- read7readN.py
+++ read7readN.py
@@ -5,6 +5,10 @@ Using a read7() method that returns 7 characters from a file, implement readN(n)
 
 For example, given a file with the content “Hello world”, three read7() returns “Hello w”, “orld” and then “”.
 
+date: sep 2019
+author: darren
+
+
 '''
 
 
