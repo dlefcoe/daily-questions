7a9ec2a945b9f1c6a77f9d536bfe767dfc4a8368
darren lefcoe <dlefcoe@gmail.com>
2019-09-25 19:26:40 +0100

    test change 02

---
+++ flefRossTestHub.py
@@ -0,0 +1,27 @@
+
+
+
+# can you see this ?
+
+
+def flefRossShare(seen):
+
+    if seen == True:
+        s = 'i have seen this'
+    else:
+        s = 'what are you talking about mate ?'
+
+    return s
+
+
+b = False
+x = flefRossShare(b)
+print(x)
+
+
+# more things done
+# test
+
+
+# blah blah
+
