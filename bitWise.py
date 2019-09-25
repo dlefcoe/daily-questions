e251a3697dd65a6c11948b5e22e4428074b0b956
darren lefcoe <dlefcoe@gmail.com>
2019-09-25 09:48:17 +0100

    Update bitWise.py

--- bitWise.py
+++ bitWise.py
@@ -11,11 +11,14 @@ You can assume b can only be 1 or 0.
 
 def bitWise(x, y, b):
 
-    r = (x * b) | (y * (b+1))
+    r = (x * b) + (y * (b^1))
 
     return r
 
-print('should print 5:')
-print(bitWise(10, 5, 0))
-print('should print 10:')
-print(bitWise(10, 5, 1))
+x = 10
+y = 6
+
+print(f'should print {y}:')
+print(bitWise(x, y, 0))
+print(f'should print {x}:')
+print(bitWise(x, y, 1))
