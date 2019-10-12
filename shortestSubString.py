a20419a45b23938fe7437cb5258c093d8b669a8e
darren lefcoe <dlefcoe@gmail.com>
2019-10-12 18:39:02 +0100

    question on ordering of the letters..

--- shortestSubString.py
+++ shortestSubString.py
@@ -7,12 +7,30 @@ For example, given the string "figehaeci" and the set of characters {a, e, i}, y
 If there is no substring containing all the characters in the set, return null.
 
 
+my question:  do we assume that the set of characters appears in the same order in the string ?
+
 '''
 
+# this is a more general method
+def shortFind(s, c):
+    '''
+    inputs:
+    s: type=string, represents the string to test
+    c: type=string, represents the list to find within the string
+
+    return:
+    sOut: type=string (or null) depending on s and c 
+
+    '''
 
 
 
+    return
 
+
+
+# this works if the letters within c occur in the same order in the string s.
+# as set out in the question above.
 def shortSubstring(s, c):
     '''
     inputs:
@@ -46,7 +64,7 @@ def shortSubstring(s, c):
         if v == c[0]:
             lastIndex = i
 
-    # first index in s of last element in c
+    # first index in s of last element in c that occurs after the lastIndex above
     firstIndex = len(s)
     for i, v in enumerate(s[lastIndex:]):
         if v == c[-1]:
