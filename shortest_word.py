7ca09dad878ff45969515b2fb0f2b61316897488
darren lefcoe <dlefcoe@gmail.com>
2023-08-09 10:25:07 +0100

    Update shortest_word.py

--- shortest_word.py
+++ shortest_word.py
@@ -1,5 +1,15 @@
 '''
-get the shortest word from a sentance in Python.
+given a sentance get the shortest word from the sentance.
+
+The shortest word is x.
+    => x
+
+    words.split('The shortest word is x.')
+    [The, shortest, word, is, x]
+    [3  , 8       , 4   , 4 , 1]
+
+    {The:3, shortest:8, word:4, is:2    ....}
+
 '''
 
 
@@ -9,19 +19,24 @@ import string
 def main():
 
     # the first method
-    s = 'The shortest word is x'
+    s = 'The shortest word is x.'
     r = shortest_word(s)
-    print(r)
+    print('the first shortest word is:', r)
 
     # the second method
-    sentence = "Hi there! This is another sample, sentence with punctuation."
+    sentence = "Hi there! This is a sample, sentence with punctuation."
     r = new_short_word(sentence)
     print('the new shortest word is:', r)
 
 
 def shortest_word(s:str):
+    ''' get shortest word '''
 
-    words = {w:len(w) for w in s.split()}
+    # remove punctuation 
+    translate = str.maketrans('', '', string.punctuation)
+    words = s.translate(translate)
+
+    words = {w:len(w) for w in words.split()}
     min_key = next(k for k,v in words.items() if v==min(words.values()))
 
     return min_key
@@ -49,8 +64,5 @@ def new_short_word(sentence:str):
 if __name__=='__main__':
     # run as the main program guard
     main()
-
-
-
 
 
