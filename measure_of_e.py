396ba86cfd830dbf2c769d7b3799e299d9ca854c
darren lefcoe <dlefcoe@gmail.com>
2023-10-17 20:02:06 +0100

    Create measure_of_e.py
    estimate the value of e probabilistically in python

---
+++ measure_of_e.py
@@ -0,0 +1,28 @@
+'''
+estimate the value of e in pyhton
+'''
+
+import random
+import math
+
+
+def value_of_e():
+    ''' get the value of e '''
+    
+    n = 1_000
+        
+    sum_count = 0
+    for i in range(n):
+        s, counter = 0, 0
+        while s <=1:
+            s += random.random()
+            counter += 1
+        sum_count += counter
+
+    e = sum_count / n
+
+    return e
+
+e = value_of_e()
+
+print(f'{e=}, difference = {math.e - e:2f}')
